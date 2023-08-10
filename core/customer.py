976817e0349efdd276056397c4f9e8572d973aea
Elliot Simmons <7a31ee@gmail.com>
2023-08-09 21:45:53 -0400

    update customer

--- core/customer.py
+++ core/customer.py
@@ -25,41 +25,41 @@ def __init__(self, email: str = None, username: str = None, password: str = None
     self.first_name = first_name
     self.last_name = last_name
 
-def get_username() -> str:
-    pass
+    def get_username() -> str:
+        return username
 
-def set_username(self, username: str) -> None:
-    pass
+    def set_username(self, username: str) -> None:
+        self.username = username    
 
-def get_password() -> str:
-    pass
+    def get_password() -> str:
+        return password
 
-def set_password(self, password: str) -> None:
-    pass
+    def set_password(self, password: str) -> None:
+        self.password = password
 
-def get_email() -> str:
-    pass
+    def get_email() -> str:
+        return email
 
-def set_email(self, email: str) -> None:
-    pass
+    def set_email(self, email: str) -> None:
+        self.email = email
 
-def get_first_name() -> str:
-    pass
+    def get_first_name() -> str:
+        return first_name
 
-def set_first_name(self, first_name: str) -> None:
-    pass
+    def set_first_name(self, first_name: str) -> None:
+        self.first_name = first_name
 
-def get_last_name() -> str:
-    pass
+    def get_last_name() -> str:
+        return last_name
 
-def set_last_name(self, last_name: str) -> None:
-    pass
+    def set_last_name(self, last_name: str) -> None:
+        self.last_name = last_name
 
-def forget_username(self, email: str) -> str:
-    pass
+    def forget_username(self, email: str) -> str:
+        pass
 
-def forget_password(self, email: str) -> str:
-    pass
+    def forget_password(self, email: str) -> str:
+        pass
 
-def generate_password_hash(self, password: str) -> str:
-    pass
+    def generate_password_hash(self, password: str) -> str:
+        pass
